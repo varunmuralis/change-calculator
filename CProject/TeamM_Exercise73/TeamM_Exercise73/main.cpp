8b9d314e9a5a96dcf00e26b5fc0225591abf5f3d
Varun Murali <varun@Varuns-MBP.hsd1.ca.comcast.net>
2025-08-05 18:01:20 -0700

    Updates

---
+++ CProject/TeamM_Exercise73/TeamM_Exercise73/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TeamM_Exercise73
+//
+//  Created by Varun Murali on 8/5/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
